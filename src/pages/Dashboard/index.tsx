c896e403b11a8ec0d48b1d0f39261c1297c2eae0
Rodrigo Antas <rodr.antas@gmail.com>
2024-08-03 18:05:32 -0300

    adding dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
+import api from '../../services';
+import { AxiosError } from 'axios';
 
 const DashboardPage: React.FC = () => {
+  const [url, setUrl] = useState('');
+  const [customAlias, setCustomAlias] = useState('');
+  const [result, setResult] = useState<any>(null);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setError('');
+
+    if (!isValidUrl(url)) {
+      setError('A URL deve começar com http:// ou https://');
+      return;
+    }
+    try {
+      const response = await api.put(`/api/create?url=${url}&CUSTOM_ALIAS=${customAlias}`);
+      setResult(response.data);
+    } catch (err) {
+      const error = err as AxiosError<{ error: string }>;
+      if (error.response) {
+        setError(error.response.data.error)
+      } else if (error.request) {
+        setError('No response from server. Please try again later.');
+      } else {
+        setError('An unexpected error occurred.');
+      }
+    }
+  };
+
+  const isValidUrl = (url: string): boolean => {
+    return url.startsWith('http://') || url.startsWith('https://');
+  };
 
   return (
     <S.Container>
-      <p>teste</p>
+      <S.Title>Encurtador de URL</S.Title>
+      <S.Form onSubmit={handleSubmit}>
+        <S.Input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Insira a URL"
+        />
+        <S.Input
+          type="text"
+          value={customAlias}
+          onChange={(e) => setCustomAlias(e.target.value)}
+          placeholder="Alias personalizado (opcional)"
+        />
+        <S.Button type="submit">Encurtar</S.Button>
+      </S.Form>
+      {error && <S.Text className="error">{error}</S.Text>}
+      {result && (
+        <div>
+          <p>URL encurtada: {result.url}</p>
+          <p>Alias: {result.alias}</p>
+          <p>Tempo de operação: {result.statistics.time_taken}</p>
+        </div>
+      )}
     </S.Container>
-
   );
 };
 
--- src/pages/Dashboard/styles.tsx
+++ src/pages/Dashboard/styles.tsx
@@ -1,5 +1,69 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 1200px;
+  width: 100%;
 `;
+
+export const Title = styled.h1`
+  color: #333;
+  margin-bottom: 1.5rem;
+  text-align: center;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Input = styled.input`
+  width: 80%;
+  padding: 0.8rem;
+  margin-bottom: 1rem;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 1rem;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-color: #007BFF;
+    outline: none;
+  }
+`
+
+
+export const Button = styled.button`
+  padding: 0.8rem 1.2rem;
+  border: none;
+  border-radius: 5px;
+  background-color: #007BFF;
+  color: #fff;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`
+
+export const Text = styled.p`
+  margin: 1rem 0;
+  color: #333;
+  text-align: center;
+
+  &.error {
+    color: #d8000c;
+    background-color: #ffbaba;
+    border: 1px solid #d8000c;
+    border-radius: 5px;
+    padding: 0.5rem;
+    width: 80%;
+    margin: 0.5rem auto;
+    text-align: left;
+  }
+
+  &.success {
+    color: green;
+  }
+`
